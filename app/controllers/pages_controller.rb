1e6f38b93666df9d4acdd696f15aceeb2d746087
Evan <srichsun@gmail.com>
2017-11-30 15:23:33 +0800

    delete unnecessary files

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,21 +1,4 @@
 class PagesController < ApplicationController
   def homepage
   end
-
-  def create
-    result = {success: true}
-  	contact = Contact.new contact_params
-  	
-    unless contact.save 
-      result = {success: false, message: 'error'}
-    end
-
-    respond_to do |format|
-      format.json { render json: result}
-    end                                                           
-  end
-
-  def contact_params
-  	params.permit(:name, :email, :message)
-  end
 end
